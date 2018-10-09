33146c73cecc09ffd796b5e98faa220302bc91f6
Yangbin Zhou <zyb@umd.com>
2018-10-09 18:07:39 -0400

    npm_scripts

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+            $('[data-toggle="tooltip"]').tooltip();
+
+            $("#mycarousel").carousel({interval:2000});
+
+            $("#carouselButton").click(function(){
+                if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                    $("#mycarousel").carousel('pause');
+                    $("#carouselButton").children("span").removeClass('fa-pause');
+                    $("#carouselButton").children("span").addClass('fa-play');
+                }
+                else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                    $("#mycarousel").carousel('cycle');
+                    $("#carouselButton").children("span").removeClass('fa-play');
+                    $("#carouselButton").children("span").addClass('fa-pause');                    
+                }
+            });
+            
+            // $("#carousel-pause").click(function(){
+            //     $('#mycarousel').carousel('pause');});
+            
+
+            // $("#carousel-play").click(function(){
+            //     $('#mycarousel').carousel('cycle');});
+            // }); 
+        });
